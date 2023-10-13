6e5691cc4c9fb2a289d591d0874d0cd224431cc9
Laptop1 <145172869+uncivilised-missile@users.noreply.github.com>
2023-10-13 17:32:16 -0600

    Update prototype1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype1.java
@@ -1,15 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Canvas;
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Concept: motor0", group = "Concept")
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Autonomous(name = "Concept: motor0", group = "Concept")
 public class prototype1 extends LinearOpMode {
     DcMotor motor0;
     DcMotor motor1;
-float RTtrigger;
+    VisionPortal portal1;
+    AprilTagProcessor aprilTag;
+    private TfodProcessor tfod;
+
+    float RTtrigger;
 float LTtrigger;
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,6 +38,25 @@ float LTtrigger;
         LTtrigger = gamepad1.left_trigger;
         motor0 = hardwareMap.get(DcMotor.class, "motor0");
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        tfod = new TfodProcessor.Builder()
+                // Use setModelAssetName() if the TF Model is built in as an asset.
+                // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                //.setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+
+                //.setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+                .build();
+        portal1 = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .addProcessor(tfod)
+                .build();
+        aprilTag = new AprilTagProcessor.Builder()
+                .build();
 
         waitForStart();
 
@@ -25,10 +64,13 @@ float LTtrigger;
             RTtrigger = gamepad1.right_trigger;
             LTtrigger = gamepad1.left_trigger;
             telemetry.addData("Trigger > ", RTtrigger);
-            if (RTtrigger   == 1) {
+            if (RTtrigger == 1) {
 
             }
-
+            //drive train, webcam, drop pixel
+            List<Recognition> currentRecognitions = tfod.getRecognitions();
+            telemetry.addData("# Objects Detected", currentRecognitions.size());
+            telemetry.update();
         }
     }
 }
