d232f2942305a26bfcde1b07ee10e8eaee28efe3
Laptop1 <145172869+uncivilised-missile@users.noreply.github.com>
2023-11-17 21:30:23 -0700

    automus first drop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/monkey_Autonomous_Red_Back.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/monkey_Autonomous_Red_Back.java
@@ -16,6 +16,10 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Autonomous(name = "Autonomous-Red-Back", group = "Concept")
 public class monkey_Autonomous_Red_Back extends LinearOpMode {
     Servo Servo0;
@@ -41,22 +45,29 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
     double zerodave = 0;
     double jeffprev = 0;
     double jeffcur = 0;
-    double jeffyes = 0;
+    double jeffyes = 100;
+    double[] jeffyes_buff = new double[100];
+    double[] daveyes_buff = new double[100];
     double zerojeff = 0;
-    boolean redmonke;
+    boolean redmonke = false;
     boolean listlength;
     double pos_x;
     double pos_y;
-    double rot;
+    double rot = 0;
     double dave_distance_left;
+    double prev_dave_distance_left;
     double dave_i = 0;
     double jeff_distance_left;
+    double prev_jeff_distance_left;
     double jeff_i = 0;
     double rot_left;
     double rot_i = 0;
     double start_time;
     double loop_time = 1;
+    double x_change = 1;
+    double y_change = 1;
     IMU imu;
+
 
 
     int state=0;
@@ -95,6 +106,11 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT)));
         imu.resetYaw();
 
+        Arrays.setAll(jeffyes_buff, i -> 100);
+        Arrays.setAll(daveyes_buff, i -> 100);
+
+        Servo0.setPosition(0.2);
+
         zerojeff = Jeff.getCurrentPosition();
         zerodave = Dave.getCurrentPosition();
 
@@ -119,19 +135,28 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
 
         while (opModeIsActive()) {
             start_time = getRuntime();
+            telemetry.addData("State > ", state);
 
-            pos_x = 0;
-            pos_y = 0;
-            rot = -90 * 3.14/180;
+            y_change = prev_jeff_distance_left - jeff_distance_left;
+            x_change = prev_dave_distance_left - dave_distance_left;
 
-            rotcur = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-            davecur = Dave.getCurrentPosition() - zerodave - rotcur * 2500;
-            jeffcur = Jeff.getCurrentPosition() - zerojeff - rotcur * 400;
+            rotcur = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+            davecur = Dave.getCurrentPosition() - zerodave - rotcur * 3.14/180 * 2500;
+            jeffcur = Jeff.getCurrentPosition() - zerojeff - rotcur * 3.14/180 * 400;
 
             telemetry.addData("Rot",rotcur*180/3.14);
 
             daveyes = davecur - daveprev; // change
             jeffyes = jeffcur - jeffprev;
+            for (int i = 1; i < jeffyes_buff.length; i++) {
+                jeffyes_buff[i] = jeffyes_buff[i-1];
+            }
+            jeffyes_buff[0] = jeffyes;
+            for (int i = 1; i < daveyes_buff.length; i++) {
+                daveyes_buff[i] = daveyes_buff[i-1];
+            }
+            daveyes_buff[0] = daveyes;
+
 
             daveyes *= 1; // offset due to build angle
             jeffyes *= 0.8;
@@ -141,11 +166,11 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
             jeff_distance_left = pos_y - jeffcur;
             jeff_i += jeff_distance_left * loop_time;
             rot_left = rot - rotcur;
-            rot_i += rot_left * loop_time;
+            rot_i += rot_left * 3.14/180 * loop_time;
 
-            double y = 0.0025*(dave_distance_left) - 0.0025 * dave_i;
-            double x = 0.0025*(jeff_distance_left) - 0.0025 * jeff_i;
-            double rx = -2.8 * rot_left + 1.8 * rot_i;
+            double y = 0.0025*(dave_distance_left);// - 0.0025 * dave_i;
+            double x = 0.0025*(jeff_distance_left);// - 0.0001 * jeff_i;
+            double rx = -1.9 * rot_left * 3.14/180;// + 0.4 * rot_i;
 
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
@@ -157,17 +182,14 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
             telemetry.addData("Jeff > ", jeffcur);
             telemetry.addData("Dave > ", davecur);
 
-            /*
+
             if (state==0) {
-                redmonke = tfod.getRecognitions().size() > 0;
+                //redmonke = tfod.getRecognitions().size() > 0;
                 if (redmonke) {
-                    state = 1;
+                    //state = 1;
                 }else {
                     state = 3;
-                    motor0.setPower(1);
-                    motor1.setPower(1);
-                    motor2.setPower(1);
-                    motor3.setPower(1) ;
+
                 }
 
             }else if (state == 1) { //drive forward
@@ -189,17 +211,17 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
                 Servo0.setPosition(0.0);
                 state = 5; // park close
             }else if (state == 3){ // move to side
-                motor0.setPower(-1);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(1);
-                if (davecur >= 8000) { // stop go to check monkey
-                    zerodave = Dave.getCurrentPosition();
-                    motor0.setPower(0);
-                    motor1.setPower(0);
-                    motor2.setPower(0);
-                    motor3.setPower(0);
-                    state = 4;
+                pos_x = 0;
+                pos_y = -3200;
+
+                if (redmonke && Arrays.stream(jeffyes_buff).sum() == 0.0 && pos_y - 40 < jeffcur && jeffcur < pos_y + 40) {
+                    zerojeff = Jeff.getCurrentPosition();
+                    pos_y = 0;
+                    //state = 4;
+                } else if (Arrays.stream(jeffyes_buff).sum() == 0.0 && pos_y - 40 < jeffcur && jeffcur < pos_y + 40) {
+                    zerojeff = Jeff.getCurrentPosition();
+                    pos_y = 0;
+                    state = 5;
                 }
             }else if (state == 4){
                 redmonke = tfod.getRecognitions().size() > 0;
@@ -209,48 +231,50 @@ public class monkey_Autonomous_Red_Back extends LinearOpMode {
                     state = 6;
                 }
             }else if (state == 5){ // dive to line
-                motor0.setPower(1);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(-1);
-                if (davecur >= 8000) { // go to drop
+                pos_x = 9251;
+                pos_y = 0;
+
+                telemetry.addData("dave buff", Arrays.stream(daveyes_buff).sum());
+
+                if (Arrays.stream(daveyes_buff).sum() == 0.0 && pos_x - 40 < davecur && davecur < pos_x + 40) {
                     zerodave = Dave.getCurrentPosition();
-                    motor0.setPower(0);
-                    motor1.setPower(0);
-                    motor2.setPower(0);
-                    motor3.setPower(0);
-                    state = 2;
+                    pos_x = 0;
+                    state = 6;
                 }
-            }else if (state == 6){ // drive forward
-                motor0.setPower(1);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(-1);
-                if (davecur >= 8000) { // go to rotate
+            }else if (state == 6){ // rotate
+                pos_x = 0;
+                pos_y = 0;
+                rot = 90;
+                if (rot - 0.5 < rotcur && rotcur < rot + 0.5) {
                     zerodave = Dave.getCurrentPosition();
-                    motor0.setPower(0);
-                    motor1.setPower(0);
-                    motor2.setPower(0);
-                    motor3.setPower(0);
+                    zerojeff = Jeff.getCurrentPosition();
                     state = 7;
                 }
-            }else if (state == 7){ // rotate
-                motor0.setPower(1);
-                motor1.setPower(1);
-                motor2.setPower(1);
-                motor3.setPower(1) ;
+            }else if (state == 7){ // forward drop
+                pos_x = 100;
+                pos_y = 0;
+
+                if (Arrays.stream(daveyes_buff).sum() == 0.0 && pos_x - 40 < davecur && davecur < pos_x + 40) {
+                    state = 8;
+                    Servo0.setPosition(0.0);
+                    pos_x = 0;
+                    zerodave = Dave.getCurrentPosition();
+                }
 
 
-            }else if (state == 8){
-
-                // drive to monkey/line3
+            }else if (state == 8){ // park
+                pos_x = -8000;
+                pos_y = 9751;
+                rot = 0;
             }
 
-             */
+
 
 
             daveprev = davecur;
             jeffprev = jeffcur;
+            prev_dave_distance_left = dave_distance_left;
+            prev_jeff_distance_left = jeff_distance_left;
 
             telemetry.addData("Dave's change", daveyes);
 
