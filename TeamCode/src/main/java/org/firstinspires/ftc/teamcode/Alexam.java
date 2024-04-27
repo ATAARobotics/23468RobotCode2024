5a8dab18de01a9b70f8d6f05d10be8b4c34ddcfa
Administrator <arinli@ataarobotics.ca>
2024-04-26 21:34:18 -0600

    drive cont

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alexam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alexam.java
@@ -1,22 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-import kotlin.math.UMathKt;
-
 @TeleOp(name="Robot: poo", group="drive")
-public class Zoax_Zoe_and_Max extends OpMode {
+public class Alexam extends OpMode {
 
     public DcMotor ship  = null;
-    public DcMotor  max  = null;
-//    public DcMotor zoe  = null;
+    public DcMotor  adam  = null;
+    public DcMotor alexis  = null;
+    public Servo temu;
+    boolean RTbumper;
     IMU imu = null;
 
 
@@ -25,8 +22,9 @@ public class Zoax_Zoe_and_Max extends OpMode {
     public void init() {
 
         ship = hardwareMap.get(DcMotor.class, "br");
-        max = hardwareMap.get(DcMotor.class, "bl");
-//        zoe = hardwareMap.get(DcMotor.class, "fr");
+        adam = hardwareMap.get(DcMotor.class, "bl");
+//        alexis = hardwareMap.get(DcMotor.class, "fr");
+        temu = hardwareMap.get(Servo.class, "bucket servo");
         imu = hardwareMap.get(IMU.class, "imu");
     }
 
@@ -37,25 +35,32 @@ public class Zoax_Zoe_and_Max extends OpMode {
 
             double spinny = -gamepad1.right_stick_x;
             double rainbow = gamepad1.left_stick_y;
+            RTbumper = gamepad1.right_bumper;
             double sped = 0.4;
-
             float fast = gamepad1.right_trigger;
+
+//          Bucket Closer and Opener servo
+            if (RTbumper) {
+                temu.setPosition(-0.1);
+            } else {
+                temu.setPosition(0.4);
+            }
+
 //            If stick is less than 1 degrees, then nothing happens on gamepad1.right_stick_x and gamepad1.right_stick_y
             if (Math.abs (gamepad1.right_stick_x) < 0.05 && (Math.abs (gamepad1.left_stick_y) < 0.05)) {
 
                 ship.setPower((-rainbow + spinny) * 0);
-                max.setPower((rainbow + spinny) * 0);
-
-            } else { if (fast > 1) {
-//                if press down on right trigger, go fast
-                ship.setPower(-rainbow + spinny);
-                max.setPower(rainbow + spinny);
+                adam.setPower((rainbow + spinny) * 0);
 
             } else {
-
-                ship.setPower((-rainbow + spinny) * sped);
-                max.setPower((rainbow + spinny) * sped);
-            }
+                if (fast == 1) {
+//                if press down on right trigger, go fast
+                    ship.setPower(-rainbow + spinny);
+                    adam.setPower(rainbow + spinny);
+                } else {
+                    ship.setPower((-rainbow + spinny) * sped);
+                    adam.setPower((rainbow + spinny) * sped);
+                }
             }
 
 
